1ac38c4360f8b5df68432b0d5d26a687201c2559
Manjunath Krishnappa <manju_25gowda@yahoo.com>
2019-12-09 16:24:56 +0530

    Changed from MVC to Web API

--- MK.IdentityServer.Host/Startup.cs
+++ MK.IdentityServer.Host/Startup.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using MK.IdentityServer;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace MK.IdentityServer.Host
 {
@@ -25,20 +25,7 @@ namespace MK.IdentityServer.Host
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.Configure<CookiePolicyOptions>(options =>
-			{
-				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
-				options.CheckConsentNeeded = context => true;
-				options.MinimumSameSitePolicy = SameSiteMode.None;
-			});
-
-
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-			//Registers IdentityServer services in DI.
-			services.AddIdentityServer()
-					.AddInMemoryClients(Host.Configuration.Clients.Get())
-					.AddInMemoryApiResources(Host.Configuration.Resources.GetApiResources());
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,23 +37,11 @@ namespace MK.IdentityServer.Host
 			}
 			else
 			{
-				app.UseExceptionHandler("/Home/Error");
 				app.UseHsts();
 			}
 
 			app.UseHttpsRedirection();
-			app.UseStaticFiles();
-			app.UseCookiePolicy();
-
-
-			app.UseIdentityServer();
-
-			app.UseMvc(routes =>
-			{
-				routes.MapRoute(
-					name: "default",
-					template: "{controller=Home}/{action=Index}/{id?}");
-			});
+			app.UseMvc();
 		}
 	}
 }
